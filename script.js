bde441bf1a36400d0590460bff76f755d31dc37d
Dr-web 20 <drwebocity20@gmail.com>
2023-05-22 02:10:36 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ var firstNameLabel = document.querySelector('#text-0000001e');
 
 firstNameInput.addEventListener('input', function() {
   // Update the label text with the entered value
-  firstNameLabel.value =  firstNameInput.value;
+  firstNameLabel.textContent = My First Name is:' + firstNameInput.value;
 });
